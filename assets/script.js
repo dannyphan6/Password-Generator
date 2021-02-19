ba8fe53922cee83b32c115255a0b3575d3d8005f
Daniel Phan <dannyphan@comcast.net>
2021-02-18 22:28:33 -0800

    Updated variables from a string to an array

--- assets/script.js
+++ assets/script.js
@@ -1,72 +1,103 @@
+// This alerts the user once you open the password generator
 window.onload = alert("Welcome to Password Generator! Click 'Ok' to begin")
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numericNumbers = "0123456789";
-var specialCharacters = "!@#$%^&*()_-+={}[];:'`~<,>.?/|";
+// Sets of data assigned to these variables
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numericNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "[", "]", ";", ":", "'", "`", "~", "<", ",", ">", ".", "?", "/", "|"];
+
 // This allows you to pull in the variable anywhere in the code
 var passwordLength;
 var verifyUppercase;
 var verifyLowercase;
 var verifySpecial;
+var verifyNumbers;
 
-
+// Function that executes these prompts, alerts, and confirms
 function getLength() {
-  var passwordLength = window.prompt("Choose a length for your password. Password length should be between 8-128 characters");
+  passwordLength = window.prompt("Choose a length for your password. Password length should be between 8-128 characters");
 
   if(passwordLength < 8) {
     window.alert("Password length must be a number between 8-128 characters");
-    getLength();
+    return;
   } else if(passwordLength > 128) {
     window.alert("Password length must be a number between 8-128 characters");
-    getLength();
+    return;
   } else if(isNaN(passwordLength)) {
     window.alert("Password length must be a number between 8-128 characters");
-    getLength();
-  } else {
+    return;
+  } 
+  verifyUppercase = confirm("Click 'Ok' to include uppercase letters in your password")
+  verifyLowercase = confirm("Click 'Ok' to include lowercase letters in your password")
+  verifySpecial = confirm("Click 'Ok' to include special letters in your password")
+  verifyNumbers = confirm("Click 'Ok' to include numbers in your password")
+
+// This creates an object where data is stored from the user 
+// The variable passwordChoice stores all of the data from passwordLength, verifyUppercase, and etc
+// On the right, is where all the data is stored
+// On the left, are names that we assign to the data that is stored
+  var passwordChoice = {
+    passwordLength: passwordLength,
+    verifyLowercase: verifyLowercase,
+    verifyUppercase: verifyUppercase,
+    verifyNumbers: verifyNumbers,
+    verifySpecial: verifySpecial
   }
-  return passwordLength;
-}
-
-function determineUppercase() {
-  window.alert("Password must include at least one uppercase letter");
-
-verifyUppercase = confirm("Please confirm by clicking 'Ok'")
-}
-
-function determineLowercase() {
-  window.alert("Password must include at least one lowercase letter");
-
-verifyLowercase = confirm("Please confirm by clicking 'Ok'")
-}
-
-function determineSpecial() {
-  window.alert("Password must include at least one special character");
-
-verifySpecial = confirm("Please confirm by clicking 'Ok'")
+return passwordChoice;
 }
 
 
-// Write password to the #password input
+
+
+// Write password to the #password input (Work on this function to add if else statements)
 function generatePassword() {
-  getLength();
-  determineUppercase();
-  determineLowercase();
-  determineSpecial();
+
+  
+  var userOptions = getLength(); 
+  console.log(userOptions.passwordLength);
+  console.log(userOptions.verifyLowercase);
+  console.log(userOptions.verifyUppercase);
+  console.log(userOptions.verifyNumbers);
+  console.log(userOptions.verifySpecial);
+  
+  if(verifyLowercase) {
+    lowerCase.push()
+  }
+  if(verifyUppercase) {
+    generateRandom += upperCase;
+  }
+  if(verifyNumbers) {
+    generateRandom += numericNumbers;
+  }
+  if(verifySpecial) {
+    generateRandom += specialCharacters;
+  }
   
   
+  
+}
+// Math.floor function
+for(var i=0; i < getLength; i++) {
+ 
+}
+
+function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-    
-  passwordText.value = password;
   
+  passwordText.value = password;
 }
+  
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
 
 
 
