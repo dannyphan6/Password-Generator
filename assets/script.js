cbb7ad8fea605a84be23b55d342338ba37dfc2ab
Daniel Phan <dannyphan@comcast.net>
2021-02-20 21:32:45 -0800

    Added comments to break down code

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ window.onload = alert("Welcome to Password Generator! Click 'Ok' to begin")
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Sets of data assigned to these variables
+// Arrays with sets of data
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numericNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -24,6 +24,7 @@ function getLength() {
     window.alert("Password length must be a number between 8-128 characters");
     return;
   } 
+
   var verifyUppercase = confirm("Click 'Ok' to include uppercase letters in your password")
   var verifyLowercase = confirm("Click 'Ok' to include lowercase letters in your password")
   var verifySpecial = confirm("Click 'Ok' to include special characters in your password")
@@ -34,11 +35,11 @@ function getLength() {
     return;
   }
 
-// This creates an object where data is stored from the user 
 // The variable passwordChoice stores all of the data from passwordLength, verifyUppercase, and etc
+// This creates an object where user data is stored 
 // On the right, is where all the data is stored
-// On the left, are names that we assign to the data that is stored
-// Key value pair (Look this up later)
+// On the left, are names that are assigned to the data that is stored
+  
   var passwordChoice = {
     passwordLength: passwordLength,
     verifyLowercase: verifyLowercase,
@@ -46,13 +47,24 @@ function getLength() {
     verifyNumbers: verifyNumbers,
     verifySpecial: verifySpecial
   }
-return passwordChoice;
+  return passwordChoice;
 }
 
+// randomArray is an argument/parameter
+// randomIndex calls for a random index number between 0 and whatever randomArray is
+// randomElement says give me the letter in the array randomArray EX: index[10] would result in the letter g
+// randomArray isn't an array yet, but an array will be assigned once the variable generateChar on line 107 is executed
+// In line 107, var generateChar is executing this function, which pulls a random index number from generateRandom, which is a full list of all the arrays put together
 
+function Random(randomArray) {
+  var randomIndex = Math.floor(Math.random() * randomArray.length)
+  var randomElement = randomArray[randomIndex]; 
+  console.log(randomIndex);
+  console.log(randomElement);
+  return randomElement;
+}
 
-
-// Write password to the #password input (Work on this function to add if else statements)
+// Write password to the #password input 
 // userOptions = passwordChoice because passwordChoice returns the user input data that's stored
 // Since userOptions = getLength, then this is saying that whatever getLength returns, is equal to userOptions
 // In this case, an object is equal to userOptions because passwordChoice is an object
@@ -67,6 +79,9 @@ function generatePassword() {
   console.log(userOptions.verifySpecial);
 
 // Creates a variable with an empty array, that will be filled with the 4 arrays above by using .push
+// var generateRandom is an array with all character types
+// var guaranteedChar is an array that guarantees at least 1 lowercase, 1 uppercase, 1 number, and 1 special character
+// var finalPassword is the final array that guarantee's at least 1 of each character type PLUS all other characters
   var generateRandom = [];
   var guaranteedChar = [];
   var finalPassword = [];
@@ -89,32 +104,25 @@ function generatePassword() {
   }
   
   for(var i=0; i < userOptions.passwordLength; i++) {
-    var generateChar = Random(generateRandom)
+    var generateChar = Random(generateRandom) 
     // generateChar is executing the function Random, which pulls a random index number from the array generateRandom, which has all of the values that was pushed
+    // This will loop as many times as the user inputted in their passwordLength selection and generate that many indexes 
     finalPassword.push(generateChar)
-    // finalPassword is a variable that 
+    // finalPassword is a variable that contains all of the 
     console.log(generateChar);
   }
 
   for(var i=0; i < guaranteedChar.length; i++) {
     finalPassword[i] = guaranteedChar[i]
-    // This is saying the first 4 index's in finalPassword is being replaced with the 4 random index's that was chosen from guaranteedChar 
+    // This is saying the first 4 elements in finalPassword is being replaced with the 4 random elements that was chosen from guaranteedChar 
+    // The guaranteedChar.length is dependent on what the user confirms. EX: If user confirms only 3 prompts, then guaranteedChar.length is 3
+    // The i is being defined in the loop, so the count will begin at 0 and increase each time it loops
   } 
 return finalPassword.join("");
 // Takes finalPassword array and converts it to a string
 
 }
 
-// characterArray is a placeholder for now
-// arrayIndex calls for a random index number
-// randomEl says give me the letter in the array characterArray of the random index number that's generated from arrayIndex EX: index[10] would result in the letter g
-function Random(randomArray) {
-  var randomIndex = Math.floor(Math.random() * randomArray.length)
-  var randomElement = randomArray[randomIndex]; 
-  console.log(randomIndex);
-  console.log(randomElement);
-  return randomElement;
-}
 
 function writePassword() {
 
